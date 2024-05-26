0d6490b40f534b3acae44362e23bef6eb5cebad7
nnourr <nourqwerty123@gmail.com>
2024-05-26 17:41:07 -0400

    add user to firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import queryString from 'query-string';
+import { SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } from './config/globals';
 
 function App() {
+  const state = "hdickalporhfsjcy";
+  const scope = "user-top-read user-read-email";
+
+  const startLoginFlow = () => {
+    window.open("https://accounts.spotify.com/authorize?" +
+    queryString.stringify({
+      response_type: "code",
+      client_id: SPOTIFY_CLIENT_ID,
+      scope: scope,
+      redirect_uri: SPOTIFY_REDIRECT_URI,
+      state: state,
+    }),"_blank")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {startLoginFlow()}}>login to spotify</button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import CallbackPage from './pages/callback/callback';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/login/callback",
+    element: <CallbackPage/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
