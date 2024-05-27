5b4e2ba5d7d95c727ba09fafe030acce7463b9fe
nnourr <nourqwerty123@gmail.com>
2024-05-26 21:28:13 -0400

    SPA implementation

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,12 @@
 import queryString from 'query-string';
-import { SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } from '../../config/globals';
+import { SERVER_ENDPOINT, SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } from '../../config/globals';
+import { useState } from 'react';
 
 function HomePage() {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
   const state = "hdickalporhfsjcy";
   const scope = "user-top-read user-read-email";
-  console.log(window.location.href);
 
   const startLoginFlow = () => {
     window.location.href = "https://accounts.spotify.com/authorize?" +
@@ -16,6 +18,54 @@ function HomePage() {
       state: state,
     })
   }
+  const url = new URL(window.location.href);
+
+  const params = new URLSearchParams(url.search);
+
+  const loginCode = params.get('code');
+  const error = params.get('error')
+  
+  if (error !== null) {
+    console.log(error);
+    return <div>login error</div>
+  }
+
+  function handleUsername(field: string) {
+    setUsername(field)
+  }
+
+  function handleEmail(field: string) {
+    setEmail(field)
+  }
+
+  const handleSubmit = async () => {
+    console.log(username);
+    
+    await fetch(SERVER_ENDPOINT+'/user/' + email, {
+      method: "POST",
+      headers: {
+      "Content-Type": "application/json",
+      },
+      body: JSON.stringify({"loginCode": loginCode, "username": username})
+    })
+  }
+
+  if (loginCode !== null) { 
+    return (
+        <div className="flex justify-center items-center">
+        <label>
+          username:
+          <input type="text" onChange={(change) => {handleUsername(change.target.value)}}/>
+        </label>
+        <label>
+          email:
+          <input type="text" onChange={(change) => {handleEmail(change.target.value)}}/>
+        </label>
+        <button type="submit" onClick={() => handleSubmit()}>submit</button>
+      </div>
+    );
+  }
+
   return (
     <div className="flex">
       <button onClick={() => {startLoginFlow()}}>login to spotify</button>
