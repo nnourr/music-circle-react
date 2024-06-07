29904ff023ad8458ac69d0c10f1cdef5a592fc49
nnourr <nourqwerty123@gmail.com>
2024-06-07 10:32:40 -0400

    turn off no unused vars

---
+++ eslint.rc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "rules": {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "error"
+    },
+    "extends": ["@babel/plugin-transform-private-property-in-object"]
+}
