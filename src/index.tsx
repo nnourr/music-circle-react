708964ebb9685b163a25ae424e5f424988e66f8f
nnourr <nourqwerty123@gmail.com>
2024-06-08 12:51:54 -0400

    set up router

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
+import {
+  HashRouter,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createHashRouter,
+} from "react-router-dom";
+import { IsMobileProvider } from "./providers/isMobile.provider";
+import { UserProvider } from "./providers/user.provider";
+import { LandingPage } from "./pages/landing/landing.page";
+import { AnotherLoginState } from "./pages/landing/states/anotherLoginState";
+import { NotFoundPage } from "./pages/notfound/notfound.page";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/home",
+    element: <AnotherLoginState />,
+  },
+]);
+
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <UserProvider>
+    <IsMobileProvider>
+      <RouterProvider router={router} />
+    </IsMobileProvider>
+  </UserProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
---
+++ src/pages/notfound/notfound.page.tsx
@@ -0,0 +1,22 @@
+import { faWarning } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "../../components/inputs/button.input.component";
+import { useNavigate } from "react-router-dom";
+import { BackgroundGradient } from "../landing/components/background-gradient.component";
+
+export const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className=" w-4/5 h-full flex items-center justify-center text-lg lg:text-lg-xl text-error m-auto flex-col">
+        <div>
+          <FontAwesomeIcon icon={faWarning} className="lg:mr-4" />
+          This is not the page you're looking for
+        </div>
+        <Button onClick={() => navigate(-1)}>go back</Button>
+      </div>
+      <BackgroundGradient />
+    </>
+  );
+};
