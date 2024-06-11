d1cc762740881049effebc74d88b2bfc3654d55f
nnourr <nourqwerty123@gmail.com>
2024-06-11 15:34:30 -0400

    animations actin up but I got it

--- src/pages/landing/components/background-gradient.component.tsx
+++ src/pages/landing/components/background-gradient.component.tsx
@@ -1,27 +1,26 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { RADIAL_GRADIENT, RADIAL_GRADIENT_ALT } from "../../../config/globals";
 
 interface BackgroundGradientProps extends React.HTMLAttributes<HTMLDivElement> {
-  alt?: boolean;
   error?: boolean;
 }
 
 export const BackgroundGradient: React.FC<BackgroundGradientProps> = ({
-  alt = false,
   error = false,
 }) => {
+  console.log(error);
+
   return (
     <motion.div
       animate={{
         backgroundImage: error
           ? "radial-gradient(50% 50% at 50% 50%, rgb(41 235 38) 0%, rgb(63 177 171) 20%, rgb(41 195 159) 100%)"
-          : alt
-          ? "radial-gradient(50% 50% at 50% 50%, rgb(41 235 38) 0%, rgb(41 195 159) 70%, rgb(63 177 171) 100%)"
-          : "radial-gradient(50% 50% at 50% 50%, rgb(41 235 38) 0%, rgb(63 177 171) 70%, rgb(41 195 159) 100%)",
+          : [RADIAL_GRADIENT_ALT, RADIAL_GRADIENT],
+        transition: { repeat: Infinity, repeatType: "mirror", duration: 5 },
       }}
       transition={{ duration: 1.5, ease: "easeOut" }}
       className="h-full w-full absolute top-0 left-0 -z-10 flex justify-center items-center overflow-hidden"
-      initial={false}
     >
       <motion.div
         className="border-green-800/30 border-[20px] h-0 w-0 rounded-[50%] blur-3xl absolute"
